38632ed0332b5e8b2882eddba0dc996a8b92de94
BruceMacD <brucewmacdonald@gmail.com>
2018-01-17 00:03:41 -0400

    isolating single score value

---
+++ README.md
@@ -0,0 +1,16 @@
+# Score Sight
+
+This is a work in progress.
+
+The goal of this application is to scan live video footage of a sports games to extract the team names and scores.
+
+Currently I am working on extracting the scores.
+
+How to build and run using SBT
+------------------------------
+
+1. Install [Java JDK](http://www.oracle.com/technetwork/java/javase/downloads/index.html)
+
+2. Install [SBT](http://www.scala-sbt.org/)
+
+TODO: add more explanation
---
+++ src/main/scala/com/bruce/scorecv/ScoreDetector.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2011-2015 Jarek Sacha. All Rights Reserved.
+ *
+ * Author's e-mail: jpsacha at gmail.com
+ */
+
+package com.bruce.scorecv
+
+import java.io.File
+
+import org.bytedeco.javacpp.opencv_imgcodecs._
+import OpenCVUtils._
+
+/**
+ * Example for section "Using the Strategy pattern in algorithm design" in Chapter 3.
+ * The pattern encapsulates an algorithm into a separate class
+ *
+ * The original example in the book is using "C++ API". Calls here use "C API" supported by JavaCV.
+ */
+object ScoreDetector extends App {
+
+  // Create image processor object
+  val colorDetector = new ColorDetector
+
+  // Read input image
+  val src = loadAndShowOrExit(new File("data/SingleScore.png"), IMREAD_COLOR)
+
+  // Set the input parameters
+  colorDetector.colorDistanceThreshold = 100
+  // here white characters
+  colorDetector.targetColor = new ColorRGB(255, 255, 255)
+
+  // Process that input image and display the result
+  val dest = colorDetector.process(src)
+  show(dest, "result")
+}
