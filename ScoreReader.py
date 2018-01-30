d45e3d281a530d884c5b337b87eabeb03fc87a42
BruceMacD <brucewmacdonald@gmail.com>
2018-01-29 23:33:07 -0400

    switch to python, tess4j was causing build problems

---
+++ ScoreReader.py
@@ -0,0 +1,39 @@
+# import the necessary packages
+from PIL import Image
+import pytesseract
+import cv2
+import os
+
+# statically set image for testing
+file_src = "data/JustFoxScore.png"
+
+
+def grayscale(src):
+    img = cv2.imread(src)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # not doing any noise removal or threshold preprocessing
+    # it makes results worse in this case
+
+    # image must be saved processing, would like a workaround, this is some overhead
+    filename = "{}.png".format(os.getpid())
+    cv2.imwrite(filename, gray)
+    return filename
+
+
+def cleanup(files):
+    # clean up files after processing
+    for file in files:
+        os.remove(file)
+
+
+def read_score(frame):
+    # TODO: find team names and score from data read
+    text = pytesseract.image_to_string(Image.open(grayscale_file))
+    print(text)
+
+if __name__ == "__main__":
+    grayscale_file = grayscale(file_src)
+    text = pytesseract.image_to_string(Image.open(grayscale_file))
+    print(text)
+    cleanup([grayscale_file])
